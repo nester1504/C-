57ece603dc57d1c39fd3d9d9c901b4803e4db25a
Alexandr Nesterenko <nesteronok@gmail.com>
2023-02-12 18:11:02 +0600

    methode

---
+++ Emample008_Method/Program.cs
@@ -0,0 +1,25 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 21;
+int b1 = 25;
+int c1 = 2121;
+int a2 = 6;
+int b2 = 234;
+int c2 = 6;
+int a3 = 0;
+int b3 = 2;
+int c3 = 1;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
