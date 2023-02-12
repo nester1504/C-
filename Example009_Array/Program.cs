88ba67bdd15ab925b39c9fa0807049f9ba29d0fa
Alexandr Nesterenko <nesteronok@gmail.com>
2023-02-12 18:22:52 +0600

    array

--- Example009_Array/Program.cs
+++ Example009_Array/Program.cs
@@ -6,16 +6,8 @@
     return result;
 }
 
-int a1 = 21;
-int b1 = 25;
-int c1 = 2121;
-int a2 = 6;
-int b2 = 234;
-int c2 = 6;
-int a3 = 0;
-int b3 = 2;
-int c3 = 1;
+int[] array = { 11, 22, 333, 44, 85, 446, 72, 887, 19 };
 
-int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
 
 Console.WriteLine(max);
