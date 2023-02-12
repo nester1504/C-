0207a442867639216661f08c307af7aea8539b24
Alexandr Nesterenko <nesteronok@gmail.com>
2023-02-12 22:54:00 +0600

    array labrary

--- Example010_MethodArray/Program.cs
+++ Example010_MethodArray/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = { 11, 22, 333, 44, 19, 85, 446, 72, 887, 19 };
+
+int n = array.Length;
+
+int find = 19;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
